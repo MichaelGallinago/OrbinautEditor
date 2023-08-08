9ebb2dafce38899c4178b14eeba5ee233f9f2be3
Michael Gallinago <michael.gallinago@gmail.com>
2023-08-08 20:46:32 +0500

    Warp TileIndex

--- CollisionEditor/ViewModel/Main/SelectorPanel/TileButtonsGrid.cs
+++ CollisionEditor/ViewModel/Main/SelectorPanel/TileButtonsGrid.cs
@@ -5,33 +5,54 @@ using System.Linq;
 
 public partial class TileButtonsGrid : GridContainer
 {
+	private const byte Separation = 4;
+	private const byte ButtonScale = 2;
+	private const double InputDelayChangeSpeed = 0.01d;
+	
 	private TileButton _hoveredTileButton;
 	private TileButton _selectedTileButton;
 	private readonly List<TileButton> _tileButtons = new();
 	private PackedScene _packedButton;
 	private Vector2I _buttonSize;
 	private ImageTexture _frameTexture;
+	private bool _isTileButtonFocused;
+	private bool _isGrabFocus;
+	private double _inputDelay;
+	private double _inputTimer;
 	
-	private const byte Separation = 4;
-	private const byte ButtonScale = 2;
+	private double InputDelay 
+	{ 
+		get => _inputDelay;
+		set => _inputDelay = Mathf.Clamp(value, 0.04d, 0.25d);
+	}
 
 	public override void _Ready()
 	{
 		_packedButton = GD.Load<PackedScene>("res://PackedObjects/tile_button.tscn");
 		
 		CollisionEditor.TileButtonsGrid = this;
-		CollisionEditor.TileIndexChangedEvents += () => _tileButtons[CollisionEditor.TileIndex].ButtonPressed = true;
-
+		CollisionEditor.TileIndexChangedEvents += () =>
+		{
+			TileButton button = _tileButtons[CollisionEditor.TileIndex];
+			button.ButtonPressed = true;
+			if (!_isGrabFocus) return;
+			button.GrabFocus();
+			_isGrabFocus = false;
+		};
+		
 		Resized += () =>
 		{
 			int buttonSpace = _buttonSize.X + Separation;
 			Columns = ((Vector2I)((Control)GetParent()).Size).X / buttonSpace;
 			Size = new Vector2(buttonSpace * Columns, Size.Y);
 		};
+
+		GetTree().Root.GuiFocusChanged += control => _isTileButtonFocused = control is TileButton;
 	}
 
 	public override void _Process(double delta)
 	{
+		InputSelectionUpdate(delta);
 		SelectedTileButtonUpdate();
 	}
 
@@ -92,7 +113,8 @@ public partial class TileButtonsGrid : GridContainer
 		var button = _packedButton.Instantiate<TileButton>();
 		button.CustomMinimumSize = _buttonSize;
 		button.TextureNormal = tile.Sprite.Texture;
-		
+		button.FocusMode = FocusModeEnum.Click;
+
 		button.Pressed += () =>
 		{
 			CollisionEditor.TileIndex = _tileButtons.IndexOf(button);
@@ -117,6 +139,63 @@ public partial class TileButtonsGrid : GridContainer
 			tileButton.Free();
 		}
 		_tileButtons.Clear();
+	}
+
+	private void InputSelectionUpdate(double delta)
+	{
+		_inputTimer -= delta;
+		if (!_isTileButtonFocused) return;
+		if (Input.IsActionPressed("left"))
+		{
+			ChangeTileIndexByInput(-1);
+		}
+		else if (Input.IsActionPressed("right"))
+		{
+			ChangeTileIndexByInput(1);
+		}
+		else if (Input.IsActionPressed("up"))
+		{
+			ChangeTileIndexByInput(-Columns);
+		}
+		else if (Input.IsActionPressed("down"))
+		{
+			ChangeTileIndexByInput(Columns);
+		}
+		else
+		{
+			InputDelay++;
+			_inputTimer = 0d;
+		}
+	}
+
+	private void ChangeTileIndexByInput(int value)
+	{
+		if (_inputTimer > 0d) return; 
+		_isGrabFocus = true;
+		
+		CollisionEditor.TileIndex = WarpTileIndex(
+			CollisionEditor.TileSet.Tiles.Count, CollisionEditor.TileIndex + value);
+		
+		InputDelay -= InputDelayChangeSpeed;
+		_inputTimer = InputDelay;
+	}
+
+	private int WarpTileIndex(int tileCount, int targetValue)
+	{
+		if (targetValue >= tileCount)
+		{
+			targetValue %= Columns;
+		}
+		else if (targetValue < 0)
+		{
+			targetValue += Mathf.CeilToInt((double)tileCount / Columns) * Columns;
+			if (targetValue >= tileCount)
+			{
+				targetValue -= Columns;
+			}
+		}
+
+		return targetValue;
 	}
 	
 	private void SelectedTileButtonUpdate()
