8ca32353d2f39c3d949a20f610321672ba75f3ae
Michael Gallinago <michael.gallinago@gmail.com>
2023-08-13 14:57:50 +0500

    Refactoring

--- CollisionEditor/ViewModel/Main/EditPanel/BigTileCanvasLine.cs
+++ CollisionEditor/ViewModel/Main/EditPanel/BigTileCanvasLine.cs
@@ -17,32 +17,38 @@ public partial class BigTileCanvasLine : Control
 		byte byteAngle = CollisionEditor.AngleMap.Angles[CollisionEditor.TileIndex];
 		(Vector2, Vector2, Vector2) positions = GetLinePositions(centre, byteAngle);
 		
-		DrawLine(positions.Item1 + centre, -positions.Item1 + centre, Colors.Red, 2);
-		DrawLine(-positions.Item1 + centre, positions.Item2 + centre, Colors.Red, 1);
-		DrawLine(positions.Item2 + centre, positions.Item3 + centre, Colors.Red, 1);
-		DrawLine(positions.Item1 + centre, positions.Item3 + centre, Colors.Red, 1);
+		DrawLine(positions.Item1 + centre, -positions.Item1 + centre, Colors.Red, 2f);
+		DrawLine(-positions.Item1 + centre, positions.Item2 + centre, Colors.Red, 2f);
+		DrawLine(positions.Item2 + centre, positions.Item3 + centre, Colors.Red, 2f);
+		DrawLine(positions.Item1 + centre, positions.Item3 + centre, Colors.Red, 2f);
 	}
 	
 	private static (Vector2, Vector2, Vector2) GetLinePositions(Vector2 centre, byte byteAngle)
 	{
-		float fullAngle = 360f - (float)Angles.GetFullAngle(byteAngle, false);
-		float origAngle = Mathf.DegToRad(fullAngle);
-		float angle = Mathf.DegToRad(fullAngle % 180);
-		float invertedAngle = Mathf.Pi / 2 - angle;
-
-		Vector2 firstCorner, secondCorner;
+		float angle = 360f - (float)Angles.GetFullAngle(byteAngle, false);
+		float fullAngle = Mathf.DegToRad(angle);
 		float cornerAngle = Mathf.Atan2(centre.X, centre.Y);
-		int sign = origAngle + 0.01f >= Mathf.Pi + cornerAngle || origAngle < cornerAngle ? 1 : -1;
+		int direction = fullAngle + 0.01f >= Mathf.Pi + cornerAngle || fullAngle < cornerAngle ? 1 : -1;
+
+		return CalculatePositions(centre, direction, angle, cornerAngle);
+	}
+
+	private static (Vector2, Vector2, Vector2) CalculatePositions(
+		Vector2 centre, int direction, float angle, float cornerAngle)
+	{
+		float halfAngle = Mathf.DegToRad(angle % 180);
+		float invertedAngle = Mathf.Pi / 2f - halfAngle;
+		Vector2 firstCorner, secondCorner;
 		if (Mathf.Abs(invertedAngle) > cornerAngle)
 		{
-			firstCorner = new Vector2(-centre.X, centre.Y * sign);
-			secondCorner = new Vector2(centre.X, centre.Y * sign);
-			centre.Y = Mathf.Tan(angle) * centre.X;
+			firstCorner = new Vector2(-centre.X, centre.Y * direction);
+			secondCorner = new Vector2(centre.X, centre.Y * direction);
+			centre.Y = Mathf.Tan(halfAngle) * centre.X;
 		}
 		else
 		{
-			firstCorner = new Vector2(centre.X * sign, -centre.Y);
-			secondCorner = new Vector2(centre.X * sign, centre.Y);
+			firstCorner = new Vector2(centre.X * direction, -centre.Y);
+			secondCorner = new Vector2(centre.X * direction, centre.Y);
 			centre.X = Mathf.Tan(invertedAngle) * centre.Y;
 		}
 
